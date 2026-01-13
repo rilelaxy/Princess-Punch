8ddba65238c4ef7d0efff73403541929441e2c42
EvanTals <evantalavera03@gmail.com>
2026-01-12 18:21:45 -0800

    Initial Game Prototype
    Room switching prototype
    Interactable semi-prototype

---
+++ GAM-120/Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] keys;
+
+    //public bool[] keysCollected = [false, false, false, false, false];
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /*public void CollectKey(int keyIndex) 
+    {
+        keysCollected[keyIndex] = true;
+        Debug.Log("Key " + keyIndex + " collected!");
+    }*/
+}
